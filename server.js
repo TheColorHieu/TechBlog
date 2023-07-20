33a559901c86110993e6139e51daf0a825bf6244
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-07-19 17:13:08 -0700

    here we set up our server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+//here we import the necessary dependencies for our server
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+const path = require('path');
+//we will be importing a helpers function for our date 
+const helpers = require('./utils/helpers');
+//in the next line of codes we will be adding handlebars 
+const handlebars = require('express-handlebars');
+const handlebars= handlebars.create({ helpers });
+//we will be adding session
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const session = {
+    secret: 'Super secret secret',
+    cookie: { maxAge: 3600 },
+    resize: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+//here we are adding the session to our server
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//we will be adding handlebars to our server
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//we will be adding routes to our server
+app.use(routes);
+//we will be adding sequelize to our server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+}
+);
